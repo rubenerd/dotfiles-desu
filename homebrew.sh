5818ee284f07ca31225d13a973b37702bc7bdb77
Ruben Schade <code@rubenschade.com>
2013-01-18 13:48:31 +1100

    Added homebrew.sh install script

---
+++ homebrew.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env sh
+
+##
+## homebrew.sh for Homebrew package manager on OS X
+## By Ruben Schade, 2012-present
+## MIT licenced
+##
+## I used pkgsrc and MacPorts for the longest time on OS X, but giving
+## Homebrew a try as my primary package manager on my MacBook Air. I
+## appreciate their effort not to duplicate software bundled with OS X,
+## but generally this software is outdated, so I install homebrew/dupes
+## first. Also, The Bird is The Word.
+##
+
+echo "INSTALLING HOMEBREW..."
+ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+
+echo "TAPPING HOMEBREW/DUPES (AVOID OLD VERSIONS SHIPPED WITH OS X)..."
+brew tap -v homebrew/dupes
+
+echo "UPDATING..."
+brew update -v
+
+echo "INSTALLING DEPENDENCIES..."
+brew install -v git-base mercurial
+
+echo "INSTALLING GOODNESS..."
+brew install -v \
+    curl \
+    dvtm \
+    flac \
+    git-extras \
+    imagemagick \
+    lame \
+    links \
+    macvim \
+    mc \
+    mcrypt \
+    perl \
+    pngcrush \
+    qemu \
+    rsync \
+    ruby \
+    vim \
+    xz
+
+echo "TAPPING MPV-PLAYER AND INSTALLING..."
+brew tap mpv-player/mpv
+brew install --HEAD mpv
+
+echo "CLEANING UP..."
+brew cleanup -v
+
+echo "LISTING INSTALLED GOODNESS..."
+brew list
+
+echo "ALL DONE. THE BIRD IS THE WORD."
