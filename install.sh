878faaad586e7b72ce89206c64ba7b51102f7e12
Ruben Schade <git@rubenschade.com>
2020-06-08 17:49:51 +1000

    Install oksh instead of bash

--- install.sh
+++ install.sh
@@ -2,17 +2,34 @@
 
 ######
 ## install.sh - Desktop orchestration installer fun
+## Easiest way to keep dotfiles in version control
 ## 2015-08
 
-## Install dotfiles
-_folder=`pwd`
-ln -s $_folder/bashrc ~/.bashrc
-ln -s $_folder/bash_profile ~/.bash_profile
-ln -s $_folder/vimrc ~/.vimrc
+linkup() {
+    if [ -f $1 ]; then
+        echo "$1 already exists."
+    else
+        ln -s $(pwd)/$1 ~/.$1
+        echo "Linked $1, which I may or may not have done like a boss."
+    fi
+}
 
-## Install MacRumors colour profile fix
-[ `uname` == "Darwin" ] && \
-    sudo cp MacBookAirLG.icc /Library/ColorSync/Profiles/Displays/
+linkup cvsrc
+linkup emacs
+linkup kshrc
+linkup oksh_completions
+linkup profile
+linkup tidyrc
+linkup vimrc
+
+case `uname` in
+    Darwin)
+        linkup shuttle.json
+        ;;
+    NetBSD)
+        linkup cvsrc
+        ;;
+esac
 
 ## EOF
 
